af6c199ed39ddfd12c3e185f4007e9438e1204b6
Szymon Milczarczyk <smilczarczyk@student.agh.edu.pl>
2022-08-29 11:38:10 +0200

    Added first lab

---
+++ lab1/zad1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+// Twój pierwszy program w języku C
+int main() {
+    printf("\e[31mWitaj Świecie!\e[0m\n");
+}
---
+++ lab1/zad3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+        float a,b,c,delta;
+        scanf("%f %f %f", &a, &b, &c);
+        printf("Rownanie (%.2f)x^2 + (%.2f)x + (%.2f) = 0 posiada rozwiazanie:\n", a, b, c);
+	delta = b*b - 4*a*c;
+	printf("Delta = %f\n",delta);
+	if ( delta > 0){
+		float x1,x2;
+		x1 = (-b + sqrt(delta))/(2*a);
+		x2 = (-b - sqrt(delta))/(2*a);
+		printf("x1 = %f, x2 = %f\n",x1,x2);
+		return 0;
+	}
+	if (delta == 0){
+		float x0= -b/(2*a);
+		printf("x0 = %f\n",x0);
+		return 0;
+	}
+	if ( delta < 0){
+		float zespo = sqrt(-delta);
+		printf("x1 = %f + %fi \n",-b/(2*a),zespo/(2*a));
+		printf("x2 = %f - %fi \n",-b/(2*a),zespo/(2*a));
+		return 0;
+	}
+}
