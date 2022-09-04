b55bd40a3f9358b23928f7bbbfae216d056b48cb
szym-i <smilczarczyk@student.agh.edu.pl>
2022-09-04 16:23:38 +0200

    Caesar Cipher where shift=length of first word

--- lab_3/1.c
+++ lab_3/1.c
@@ -19,7 +19,7 @@ char* getEncryptedLine(char* string) // Caesar Cipher, where shift/key = len of 
 		if((string[i] != ' ') && (string[i] != '\0') && (string[i] != '\n')){
 			d = string[i];
 			c = string[i];
-			x = c + s % 26;// % 26 in case shift is over 25
+			x = c + s % 26; // % 26 in case shift is over 25
 			if (( d > 64 ) && ( d < 91)){  // for upper case letters
 				if ( x > 90)
 					x = x - 26;
